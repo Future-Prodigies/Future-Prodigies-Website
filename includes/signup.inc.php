abd09b25cc6bb6ed79eeef3605c8ef9bb48f08f2
RoJuan230 <rohan.nakra100@gmail.com>
2022-07-25 16:21:48 -0700

    Adding error handlers.

--- includes/signup.inc.php
+++ includes/signup.inc.php
@@ -6,7 +6,35 @@ if (isset($_POST['submit'])) {
     $username = $_POST['uid'];
     $pwd = $_POST['pwd'];
     $pwdRepeat = $_POST['pwdrepeat'];
+
+    require_once 'dbh.inc.php';
+    require_once 'functions.inc.php';
+
+    if (emptyInputSignup($name, $email, $username, $pwd, $pwdRepeat) !== false) {
+        header("location: ../signup.php?error=emptyinput");
+        exit();
+    }
+    if (invalidUid($username) !== false) {
+        header("location: ../signup.php?error=invaliduid");
+        exit();
+    }
+    if (InvalidEmail($email) !== false) {
+        header("location: ../signup.php?error=invalidemail");
+        exit();
+    }
+    if (pwdMatch($pwd, $pwdRepeat) !== false) {
+        header('location: ../signup.php?error=passwordsdontmatch');
+        exit();
+    }
+    if (uidExists($conn, $usename) !== false) {
+        header('location: ../signup.php?error=usernametaken');
+        exit();
+    }
+
+    createUser($conn, $name, $email, $username, $pwd)
+
 }
 else {
     header("location: ../signup.php");
+    exit();
 }
